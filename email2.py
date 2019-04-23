3606b9e319ad8f66efa1c8c18eb95d55ca35c939
Sourav Jangra <imsouravjangra@gmail.com>
2019-04-23 11:31:51 +0530

    email fix!

--- email2.py
+++ email2.py
@@ -11,8 +11,8 @@ s.starttls()
 try:
 	s.login("apjasuian@gmail.com", "asu@12345")# sender email and password
 	# message to be sent 
-	message =open('new.txt').read()
-	print(message)
+	message =open('messages.txt').read()
+	# print(message)
 	# sending the mail 
 	BODY = '\r\n'.join(['To: %s' %"imsouravjangra@gmail.com" ,'From: %s' % "apjasuian@gmail.com",'Subject: %s' % "chats archieved",'', message])
 	s.sendmail("sender_email_id", "imsouravjangra@gmail.com", BODY)
